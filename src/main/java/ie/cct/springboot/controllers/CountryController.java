588f6df5293154eefb3b28c00ee63a1f049f7691
Rubenvaldo desktop <rubo.rfn@gmail.com>
2020-10-31 17:02:57 +0000

    Deleting a country record
    Steps to write service and controller methods to delete a country record.
    
    Modal from Boostrap official webpage
    
    coutrny.js

--- src/main/java/ie/cct/springboot/controllers/CountryController.java
+++ src/main/java/ie/cct/springboot/controllers/CountryController.java
@@ -1,11 +1,16 @@
 package ie.cct.springboot.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ie.cct.springboot.models.Country;
 import ie.cct.springboot.services.CountryService;
@@ -25,6 +30,34 @@ public class CountryController {
 		model.addAttribute("countries", countryList);
 		
 		return "country";
+	}
+	
+	
+	@PostMapping("/countries/addNew")
+	public String addNew(Country country) {
+		countryService.save(country);
+		return "redirect:/countries";
+	}
+	
+	
+	@RequestMapping("countries/findById")
+	@ResponseBody // to not return a page
+	public Optional<Country> findById(int id) {
+		return countryService.findById(id);
+		
+	}
+	
+	@RequestMapping(value="/countries/update", method= {RequestMethod.PUT, RequestMethod.GET})
+	public String update(Country country) {
+		countryService.save(country);
+		return "redirect:/countries";
+	}
+	
+	
+	@RequestMapping(value="/countries/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+	public String delete(Integer id) {
+		countryService.delete(id);
+		return "redirect:/countries";
 	}
 	
 	
--- src/main/java/ie/cct/springboot/services/CountryService.java
+++ src/main/java/ie/cct/springboot/services/CountryService.java
@@ -7,6 +7,7 @@ import ie.cct.springboot.models.Country;
 import ie.cct.springboot.repositories.CountryRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CountryService {
@@ -17,6 +18,21 @@ public class CountryService {
 	// Return list of countries
 	public List<Country> getCountries(){
 		return countryRepository.findAll(); 
+		
+	}
+	
+	// Save new country
+	public void save (Country country) {
+		countryRepository.save(country);
+	}
+	
+	// Get by id
+	public Optional<Country> findById(int id) {
+		return countryRepository.findById(id);
+	}
+
+	public void delete(Integer id) {
+		countryRepository.deleteById(id);
 		
 	}
 
